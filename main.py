3acb8f37a9ee15187da2f0fd72529417bc7e034a
oid85 <oid85@mail.ru>
2025-08-07 20:26:04 +0700

    ,

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+from pdf2docx import Converter
+
 if __name__ == "__main__":
-    pass
+    # Specify the PDF file location
+    pdf_file = r"o:\Книги\Переводы\Pole A. Statistical arbitrage.. Algorithmic trading insights and techniques (Wiley, 2007)(ISBN 0470138440)(257s)_FT_.pdf"
+
+    # Specify the output DOCX file location
+    docx_file = r"o:\Книги\Переводы\Pole A. Statistical arbitrage.. Algorithmic trading insights and techniques (Wiley, 2007)(ISBN 0470138440)(257s)_FT_.docx"
+
+    # Convert the PDF file to a DOCX file
+    cv = Converter(pdf_file)
+    cv.convert(docx_file)
+    cv.close()
